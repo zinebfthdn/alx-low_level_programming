236f6af8feb4f0587fa44fff0ecb368781aa5df1
zinebfthdn <zineb.fetheddine.15@gmail.com>
2023-04-20 14:42:17 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 /**
-* main - beginning of the code
+*main - beginning of the code
 *
-* Return: Always 0 
+*Return: Always 0 
 */
 int main(void)
 {
 
-puts("Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
         
 }
