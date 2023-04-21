4a3d915b9ead3ce46cf52291067fce6bbf2a2e4c
zinebfthdn <zineb.fetheddine.15@gmail.com>
2023-04-21 08:53:52 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,20 +6,19 @@
 *if the number is less than 0: is negative
 */
 int main(){
-
 int n;
 printf("Write a number :");
 scanf("%d",n);
 if (n>0){
-printf("%d is positive"), n};
+printf("%d is positive", n);
 }
 else if (n<0)
 {
-printf("%d is negative"), n};
+printf("%d is negative", n);
 }
 else if (n==0)
 {
-printf("%d is zero"), n};
+printf("%d is zero", n);
 }
 return (n);
 }
