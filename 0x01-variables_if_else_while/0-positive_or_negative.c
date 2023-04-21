5814a05476012746f11acc3f2d6095118938fd04
zinebfthdn <zineb.fetheddine.15@gmail.com>
2023-04-21 08:49:09 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+*if the number is greater than 0: is positive
+*if the number is 0: is zero
+*if the number is less than 0: is negative
+*/
+int main(){
+
+int n;
+printf("Write a number :");
+scanf("%d",n);
+if (n>0){
+printf("%d is positive"), n};
+}
+else if (n<0)
+{
+printf("%d is negative"), n};
+}
+else if (n==0)
+{
+printf("%d is zero"), n};
+}
+return (n);
+}
+
