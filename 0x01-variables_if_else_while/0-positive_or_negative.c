c197e76569cf89c8b7b695c23059b5aca0d9bd8d
zinebfthdn <zineb.fetheddine.15@gmail.com>
2023-04-21 10:09:20 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 /**
-*main: beginning of the code
+*main -  beginning of the code
 *if the number is greater than 0: is positive
 *if the number is 0: is zero
 *if the number is less than 0: is negative
