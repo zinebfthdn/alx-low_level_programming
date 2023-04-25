9c1b3f20a1d16102a93edbd573bcebedd36e253f
zinebfthdn <zineb.fetheddine.15@gmail.com>
2023-04-25 11:34:04 -0700

    t0t

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,21 @@
 #include "main.h"
 /**
- * main-begin to code
+ * main - begin to code
+ *
  * return : always 0
  */
 int main(void)
 {
 
-_putcha('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
+_putchar('_');
+_putchar("p");
+_putchar("u");
+_putchar("t");
+_putchar("c");
+_putchar("h");
+_putchar("a");
+_putchar("r");
+_putchar("\n");
 return (0);
 
 }
