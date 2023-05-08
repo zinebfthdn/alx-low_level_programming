ebe963683a80996aa8bd05961f90bd393a7fbdf5
zinebfthdn <zineb.fetheddine.15@gmail.com>
2023-05-08 10:58:32 -0700

    advanced

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * set_string - put a pointer to a character
+ * @s: pointer a  pointer
+ * @to: pointer un character
+ */
+void set_string(char **s, char *to)
+{
+*s = to;
+}
