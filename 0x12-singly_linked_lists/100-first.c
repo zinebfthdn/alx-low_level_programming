05f5e5c20e91a7a8e3494618c9d1c083e28085ac
zinebfthdn <zineb.fetheddine.15@gmail.com>
2023-06-01 12:56:22 -0700

    advanced

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void The_Hare_and_the_Tortoise(void) __attribute__(());
+/**
+ * first - ecrire une certaine phrase
+ * une telle fonction
+ */
+void The_Hare_and_the_Tortoise(void)
+{
+printf("You're beat! and yet, you must allow,\n I bore my house upon my back!\n");
+}
