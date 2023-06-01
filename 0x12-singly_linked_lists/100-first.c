5284ca25f37c7f55e50841cd799a08a084792cfe
zinebfthdn <zineb.fetheddine.15@gmail.com>
2023-06-01 13:00:13 -0700

    adv

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-void The_Hare_and_the_Tortoise(void) __attribute__(());
+void The_Hare_and_the_Tortoise(void) __attribute__((constructor));
 /**
  * first - ecrire une certaine phrase
  * une telle fonction
  */
 void The_Hare_and_the_Tortoise(void)
 {
-printf("You're beat! and yet, you must allow,\n I bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
 }
