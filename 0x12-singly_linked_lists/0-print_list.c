edb9950f53c68366233322991767aa7ea33f2c8f
zinebfthdn <zineb.fetheddine.15@gmail.com>
2023-06-01 11:33:06 -0700

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,4 +1,4 @@
-#include “lists.h”
+#include 'lists.h'
 #include <stdio.h>
 /**
  * print_list - ecrire une liste
@@ -11,15 +11,15 @@ size_t print_list(const list_t *h)
 size_t lol = 0;
 while (h)
 {
-if (!h->str)
+if (!h -> str)
 {
 printf("[0] (nil)\n");
 }
 else
 {
-printf("[%u]%s\n", h->len, h->str);
+printf("[%u]%s\n", h -> len, h -> str);
 }
-h = h->next;
+h = h -> next;
 lol++;
 }
 return (lol);
