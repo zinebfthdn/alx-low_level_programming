8ea2502285098872c70812a82ea713d922411f75
zinebfthdn <zineb.fetheddine.15@gmail.com>
2023-06-01 12:15:26 -0700

    task2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * add_node - ajouter un noeuds
+ * @head: un autre pointeur
+ * @str: string
+ *
+ * Return: adress si vrai, contraire si faux
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *nouveau;
+	unsigned int length = 0;
+
+	while (str[length])
+		length++;
+
+	nouveau  = malloc(sizeof(list_t));
+	if (!nouveau)
+		return (NULL);
+
+	nouveau->str = strdup(str);
+	nouveau->length = length;
+	nouveau->next = (*head);
+	(*head) = nouveau;
+
+	return (*head);
+}
