3f990fabf126961530a3acc70f5ae235e7f919ec
zinebfthdn <zineb.fetheddine.15@gmail.com>
2023-06-06 08:09:20 -0700

    task10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -2,7 +2,7 @@
 
 /**
  * delete_nodeint_at_index - supprimer un noeud
- * @head: a pointer 
+ * @head: a pointer
  * @index: index
  *
  * Return: 1 if true, -1 if false
@@ -32,7 +32,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 
 
-	now = tp ->next;
+	now = tp->next;
 	tp->next = now->next;
 	free(now);
 
