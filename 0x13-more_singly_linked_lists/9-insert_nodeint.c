24316f008e1d7df76b0c90062c3a61353ef87f39
zinebfthdn <zineb.fetheddine.15@gmail.com>
2023-06-06 08:00:55 -0700

    t

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *nouveau;
 	listint_t *tp = *head;
 
-	nouveau= malloc(sizeof(listint_t));
+	nouveau = malloc(sizeof(listint_t));
 	if (!nouveau || !head)
 		return (NULL);
 
