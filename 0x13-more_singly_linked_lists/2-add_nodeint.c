528ffd4acb64c2f899667a6d4772b627196a0f4f
zinebfthdn <zineb.fetheddine.15@gmail.com>
2023-06-06 07:39:07 -0700

    task

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -11,7 +11,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *nouveau;
 
-	nouveau= malloc(sizeof(listint_t));
+	nouveau = malloc(sizeof(listint_t));
 	if (!nouveau)
 		return (NULL);
 
