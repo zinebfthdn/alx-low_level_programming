e8361f4fb999fcb5be036f090cd69a6e86fa1bf7
zinebfthdn <zineb.fetheddine.15@gmail.com>
2023-06-12 13:44:46 -0700

    task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,7 +21,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 
 	op = open(filename, O_WRONLY | O_APPEND);
-	num = write(o, text_content, length);
+	num = write(op, text_content, length);
 
 	if (op == -1 || num == -1)
 		return (-1);
